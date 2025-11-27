448d96980492be9c0ec5e09f24b314442c4059cc
wish4natty-cmd <wish4natty@gmail.com>
2025-11-27 14:29:48 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ import os
 
 
 url = "https://n-production-serial-code.sekai-en.com/api/serial-code"
-user_id = "413727xxxxxxxxxxxx"  # replace with your actual user ID if needed
+user_id = "677869293366751238"  # replace with your actual user ID if needed
 
 headers = {
     "Host": "n-production-serial-code.sekai-en.com",
